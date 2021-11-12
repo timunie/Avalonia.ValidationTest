b73dd1585f33de663fe3c117477ac12104b26a6c
Tobias Johansson <kvalitetskontroll@outlook.com>
2021-11-12 10:08:58 +0100

    Adds DataAnnotation attributes to Project.

--- Avalonia.ValidationTest.Model/Project.cs
+++ Avalonia.ValidationTest.Model/Project.cs
@@ -1,12 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Avalonia.ValidationTest.Model
 {
+    // DataAnnotation attributes in this class are not used for validation i app
     public class Project
     {
         public int Id { get; set; }
+        
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+        
+        [Required]
+        [StringLength(20)]
         public string Number { get; set; }
+        
+        [Required]
+        [StringLength(50)]
         public string Remark { get; set; }
+        
+        [Required]
+        [StringLength(20)]
         public string Select { get; set; }
+        
         public bool IsChecked { get; set; }
     }
 }
